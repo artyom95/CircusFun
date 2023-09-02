f33413fcf4e226fb2fdb61b2a42ebb4db32fc020
artyom95 <92054262+artyom95@users.noreply.github.com>
2023-09-02 18:11:17 +0300

    refactor: refactored GenerateBG class

---
+++ Assets/Scripts/BackGroundController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Serialization;
+
+/*
+ * 	This script is used to generate the background repeatedly. 
+ */ 
+
+public class BackGroundController : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject _backGround;	//	The background prefab which should be generated repeatedly.
+
+	// Use this for initialization
+	private void Start () 
+	{
+		InvokeRepeating (nameof(InstanceBackGround),0.03f, 5.0f);
+	}
+
+	private void InstanceBackGround()
+	{
+		Instantiate (_backGround);
+	}
+}
